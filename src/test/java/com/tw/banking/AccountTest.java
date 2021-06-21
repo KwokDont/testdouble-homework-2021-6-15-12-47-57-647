b75c19ebfc072cb2331dc315c00b74f6381a77a7
KwokDont <39146657+KwokDont@users.noreply.github.com>
2021-06-22 00:00:42 +0800

    Test cover Account add withdraw func

--- src/test/java/com/tw/banking/AccountTest.java
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -20,4 +20,19 @@ class AccountTest {
         //then
         verify(transactionRepository, times(1)).addDeposit(amount);
     }
+
+    @Test
+    void should_transaction_repo_add_withdraw_been_call_when_execute_withdraw() {
+        //given
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+        Printer printer = mock(Printer.class);
+        Account account = new Account(transactionRepository, printer);
+        int amount = 1000;
+
+        //when
+        account.withdraw(amount);
+
+        //then
+        verify(transactionRepository, times(1)).addWithdraw(amount);
+    }
 }
