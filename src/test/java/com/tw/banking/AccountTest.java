aa14490d974d927360a91d60cd3b08cc4c3d6c50
KwokDont <39146657+KwokDont@users.noreply.github.com>
2021-06-21 23:56:16 +0800

    Test cover Account add deposit func

---
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -0,0 +1,23 @@
+package com.tw.banking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class AccountTest {
+
+    @Test
+    void should_transaction_repo_add_deposit_been_call_when_execute_deposit() {
+        //given
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+        Printer printer = mock(Printer.class);
+        Account account = new Account(transactionRepository, printer);
+        int amount = 1000;
+
+        //when
+        account.deposit(amount);
+
+        //then
+        verify(transactionRepository, times(1)).addDeposit(amount);
+    }
+}
