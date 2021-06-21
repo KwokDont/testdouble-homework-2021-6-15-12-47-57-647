e1483af2060f658114c03d17b8b3d0088bd7e9d5
KwokDont <39146657+KwokDont@users.noreply.github.com>
2021-06-21 23:53:41 +0800

    Test cover TransactionRepository allTransactions func

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -2,6 +2,8 @@ package com.tw.banking;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -34,5 +36,23 @@ class TransactionRepositoryTest {
         //then
         assertEquals(1, transactionRepository.transactions.size());
         assertEquals(-100, transactionRepository.transactions.get(0).amount());
+    }
+
+    @Test
+    void should_return_correct_trasaction_list_when_allTransactions_execute() {
+        //given
+        int amount = 100;
+        Clock mockClock = mock(Clock.class);
+        TransactionRepository transactionRepository = new TransactionRepository(mockClock);
+
+        //when
+        transactionRepository.addWithdraw(amount);
+        transactionRepository.addDeposit(amount);
+        List<Transaction> transactions = transactionRepository.allTransactions();
+
+        //then
+        assertEquals(2, transactions.size());
+        assertEquals(-100, transactions.get(0).amount());
+        assertEquals(100, transactions.get(1).amount());
     }
 }
